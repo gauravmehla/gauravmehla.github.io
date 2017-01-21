0847f329fb3c254ba78d052641173da5a541a2ac
Gaurav <gaurav.ror007@gmail.com>
2017-01-21 13:57:50 +0530

    Handlebars integrated. Terminal on landing page.

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var exphbs  = require('express-handlebars');
+var hbs = exphbs.create({ /* config */ });
+
+
 var index = require('./routes/index');
-var users = require('./routes/users');
+var about = require('./routes/about');
+var timeline = require('./routes/timeline');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,24 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/me', about);
+app.use('/timeline', timeline);
 
 module.exports = app;
