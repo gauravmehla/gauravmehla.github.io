74eb090a3764ebea59621be78c9d0a9b14ba328e
Gaurav <gaurav.ror007@gmail.com>
2017-05-26 07:15:46 +0530

    favicon added.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
